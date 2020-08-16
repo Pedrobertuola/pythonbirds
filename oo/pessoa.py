08edd560264e03c33dad76402fb81cc04e0281fb
thejoker <pedrobertuola@gmail.com>
2020-08-16 18:49:04 -0300

    Implementado carro.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,13 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return  42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+
 if __name__=='__main__':
     renzo=Pessoa(nome='Renzo')
     luciano=Pessoa(renzo,nome='Luciano')
@@ -28,3 +35,4 @@ if __name__=='__main__':
     print(luciano.olhos)
     print(renzo.olhos)
     print(id(Pessoa.olhos),id(luciano.olhos),id(renzo.olhos))
+    print(Pessoa.metodo_estatico(),luciano.metodo_estatico())
